7e961a42c889e095967661b7499b6321641ab22a
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-12-18 17:34:15 +0900

    Add: textfield add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { Button, TextField, Typography } from '@mui/material'
+import { Box } from '@mui/system'
 import TodoList from 'src/components/TodoList'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <>
       <TodoList />
-    </div>
+
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <Button variant="contained">Add</Button>
+      <Button variant="contained" color="error">
+        delete
+      </Button>
+
+      <Box>
+        <Typography>残りのタスク</Typography>
+      </Box>
+    </>
   )
 }
 

a789aacdbdc6f4b763e830396bf1ccfc7c23cd3b
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-12-18 17:57:32 +0900

    Update: usestate change

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
-import { Button, TextField, Typography } from '@mui/material'
+import { Button, Container, TextField, Typography } from '@mui/material'
 import { Box } from '@mui/system'
+import { useState } from 'react'
 import TodoList from 'src/components/TodoList'
 import './App.css'
 
 function App() {
+  const [todoList, setTodos] = useState([
+    { id: 1, name: 'todo', completed: false },
+    { id: 2, name: 'todo1', completed: false },
+    { id: 3, name: 'todo2', completed: false },
+    { id: 4, name: 'todo3', completed: false }
+  ])
+
   return (
     <>
-      <TodoList />
+      <Container maxWidth="sm">
+        <Box mt={4} mb={4}>
+          <TodoList todoList={todoList} />
+        </Box>
 
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <Button variant="contained">Add</Button>
-      <Button variant="contained" color="error">
-        delete
-      </Button>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <Button variant="contained">Add</Button>
+        <Button variant="contained" color="error">
+          delete
+        </Button>
 
-      <Box>
-        <Typography>残りのタスク</Typography>
-      </Box>
+        <Box>
+          <Typography>残りのタスク</Typography>
+        </Box>
+      </Container>
     </>
   )
 }

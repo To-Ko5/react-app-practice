7ce2f1854b00cc803da2b1f0ffd803077c207e56
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-12-20 23:07:12 +0900

    Add: textの入力を取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import { Button, Container, TextField, Typography } from '@mui/material'
 import { Box } from '@mui/system'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import TodoList from 'src/components/TodoList'
 import './App.css'
 
 function App() {
-  const [todoList, setTodos] = useState([
+  const [todoList, setTodoList] = useState([
     { id: 1, name: 'todo', completed: true },
     { id: 2, name: 'todo1', completed: false },
     { id: 3, name: 'todo2', completed: false },
     { id: 4, name: 'todo3', completed: false }
   ])
+  const [text, setText] = useState('')
+
+  const handleInput = (e: string) => {
+    setText(e)
+  }
+
+  const handleAddTodo = () => {
+    setTodoList((prevTodoList) => {
+      return [...prevTodoList, { id: 2, name: text, completed: false }]
+    })
+    setText('')
+  }
 
   return (
     <>
@@ -19,8 +31,16 @@ function App() {
           <TodoList todoList={todoList} />
         </Box>
 
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <Button variant="contained">Add</Button>
+        <TextField
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+          value={text}
+          onChange={(e) => handleInput(e.target.value)}
+        />
+        <Button variant="contained" onClick={handleAddTodo}>
+          Add
+        </Button>
         <Button variant="contained" color="error">
           delete
         </Button>

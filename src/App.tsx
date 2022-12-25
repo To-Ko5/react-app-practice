7a34426e2b143361ebb189bd5c0cea689d96ca82
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-12-25 10:21:20 +0900

    Add: not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import About from 'src/components/about/About'
 import Blog from 'src/components/blog/Blog'
+import NotFound from 'src/components/common/NotFound'
+import Gallery from 'src/components/gallery/Gallery'
 import TodoForm from 'src/components/todo/TodoForm'
 
 import './App.css'
@@ -11,6 +13,8 @@ const App = () => (
       <Route path="/" element={<TodoForm />} />
       <Route path="/about" element={<About />} />
       <Route path="/blog/:id" element={<Blog />} />
+      <Route path="/gallery" element={<Gallery />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
 )

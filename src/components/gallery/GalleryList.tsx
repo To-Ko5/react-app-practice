9ce75e7381c087abb927a2fb4ff859f2d81b74ca
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-12-25 21:37:07 +0900

    Add: useCallbackを使用

--- src/components/gallery/GalleryList.tsx
+++ src/components/gallery/GalleryList.tsx
@@ -1,6 +1,6 @@
 import Masonry from '@mui/lab/Masonry'
 import { Box, Pagination } from '@mui/material'
-import { FC, useState } from 'react'
+import { FC, useCallback, useState } from 'react'
 import ReactPaginate from 'react-paginate'
 import GalleryImage from 'src/components/gallery/GalleryImage'
 import { Gallery } from 'src/types/gallery'
@@ -17,17 +17,23 @@ const GalleryList: FC<Props> = (props) => {
   const currentGalleries = galleries.slice(galleriesOffset, endOffset)
   const pageCount = Math.ceil(galleries.length / showGalleryCount)
 
-  const handlePageClick = (e: { selected: number }) => {
-    const newOffset = (e.selected * showGalleryCount) % galleries.length
-    setGalleriesOffset(newOffset)
-  }
+  const handlePageClick = useCallback(
+    (e: { selected: number }) => {
+      const newOffset = (e.selected * showGalleryCount) % galleries.length
+      setGalleriesOffset(newOffset)
+    },
+    [galleries]
+  )
 
-  const handlePageClickMui = (page: number) => {
-    const beforePage = page - 1
-    setPage(page)
-    const newOffset = (beforePage * showGalleryCount) % galleries.length
-    setGalleriesOffset(newOffset)
-  }
+  const handlePageClickMui = useCallback(
+    (page: number) => {
+      const beforePage = page - 1
+      setPage(page)
+      const newOffset = (beforePage * showGalleryCount) % galleries.length
+      setGalleriesOffset(newOffset)
+    },
+    [galleries]
+  )
 
   return (
     <>
@@ -42,7 +48,7 @@ const GalleryList: FC<Props> = (props) => {
       <Pagination
         count={pageCount}
         page={page}
-        onChange={(e, page) => handlePageClickMui(page)}
+        onChange={(_e, page) => handlePageClickMui(page)}
       />
     </>
   )

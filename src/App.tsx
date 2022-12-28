ba9e92eb8dd1aacb092c5238db874d15ca416f5c
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-12-28 09:12:17 +0900

    Add: search page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Blog from 'src/components/blog/Blog'
 import NotFound from 'src/components/common/NotFound'
 import Gallery from 'src/components/gallery/Gallery'
 import HooksForm from 'src/components/hooks-form/HooksForm'
+import SearchApp from 'src/components/search-app/SearchApp'
 import TodoForm from 'src/components/todo/TodoForm'
 
 import './App.css'
@@ -16,6 +17,7 @@ const App = () => (
       <Route path="/blog/:id" element={<Blog />} />
       <Route path="/gallery" element={<Gallery />} />
       <Route path="/hooks-form" element={<HooksForm />} />
+      <Route path="/search-app" element={<SearchApp />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>

b60f1f87840454740ee0b45769ea931c9587a63c
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2023-01-03 22:01:08 +0900

    Add: checkbox controller

--- src/components/login/LoginCheckBox.tsx
+++ src/components/login/LoginCheckBox.tsx
@@ -1,12 +1,26 @@
 import { Checkbox, FormControlLabel } from '@mui/material'
 import React from 'react'
+import { Controller, useFormContext } from 'react-hook-form'
 
 const LoginCheckBox = () => {
+  const {
+    control,
+    formState: { errors }
+  } = useFormContext()
+
   return (
     <>
-      <FormControlLabel
-        control={<Checkbox name="login" />}
-        label="ログイン状態を保持する"
+      <Controller
+        name="loginedCheck"
+        control={control}
+        defaultValue={false}
+        render={({ field }) => (
+          <FormControlLabel
+            {...field}
+            control={<Checkbox name="loginedCheck" />}
+            label="ログイン状態を保持する"
+          />
+        )}
       />
     </>
   )

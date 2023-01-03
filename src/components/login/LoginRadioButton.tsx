3b0f543c812428eb3bc727f08749078b67f81d6d
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2023-01-03 22:08:26 +0900

    Add: login radio controller

--- src/components/login/LoginRadioButton.tsx
+++ src/components/login/LoginRadioButton.tsx
@@ -5,22 +5,42 @@ import {
   Radio,
   RadioGroup
 } from '@mui/material'
+import { Controller, useFormContext } from 'react-hook-form'
 
 const LoginRadioButton = () => {
+  const {
+    control,
+    formState: { errors }
+  } = useFormContext()
+
   return (
     <>
-      <FormControl>
-        <FormLabel id="gender-group-label">Gender</FormLabel>
-        <RadioGroup
-          row
-          aria-labelledby="gender-group-label"
-          name="gender-buttons-group"
-        >
-          <FormControlLabel value="female" control={<Radio />} label="Female" />
-          <FormControlLabel value="male" control={<Radio />} label="Male" />
-          <FormControlLabel value="other" control={<Radio />} label="Other" />
-        </RadioGroup>
-      </FormControl>
+      <Controller
+        name="gender"
+        control={control}
+        defaultValue=""
+        rules={{
+          required: { value: true, message: '必須入力です' }
+        }}
+        render={({ field, fieldState }) => (
+          <FormControl {...field}>
+            <FormLabel id="gender-group-label">Gender</FormLabel>
+            <RadioGroup row aria-labelledby="gender-group-label" name="gender">
+              <FormControlLabel
+                value="female"
+                control={<Radio />}
+                label="Female"
+              />
+              <FormControlLabel value="male" control={<Radio />} label="Male" />
+              <FormControlLabel
+                value="other"
+                control={<Radio />}
+                label="Other"
+              />
+            </RadioGroup>
+          </FormControl>
+        )}
+      />
     </>
   )
 }

c047d38c525b896e44c84402c6d2e091bfc7db3c
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2023-01-07 11:12:05 +0900

    Add: routeのガードを追加

--- src/components/login/LoginComplete.tsx
+++ src/components/login/LoginComplete.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { useUser } from 'src/context/UserContext'
 
 const LoginComplete = () => {
+  const navigate = useNavigate()
   const { user, isLoading } = useUser()
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login')
+    }
+  }, [])
 
   return <div>{user && user.name}</div>
 }
